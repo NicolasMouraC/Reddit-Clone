4b97bbf1fe1b777995e53e09372e14409aa7d357
Nicolas <nicolasoutog@gmail.com>
2022-12-24 12:50:11 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className='App'>
+      <header>
+        <div className='navBar'>
+          <h1 className='title'>asa</h1>
+        </div>
+      </header>
+
+      <section className='toolbar'>
+        toolbar
+      </section>
+
+      <section className='hot-posts'>
+        <div className='hot-post'>
+          hot post
+        </div>
+        <div className='hot-post'>
+          hot post
+        </div>
+        <div className='hot-post'>
+          hot post
+        </div>
+        <div className='hot-post'>
+          hot post
+        </div>
+      </section>
+
+      <section className='post'>
+        post
+      </section>
+      
+      <section className='bar'>
+        bar
+      </section>
+    </main>
+    
+  );
+}
+
+/*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +55,6 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
+    </div>*/
 
 export default App;
