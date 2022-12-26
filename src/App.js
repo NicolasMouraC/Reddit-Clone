caf284d05b322cc5fec985d6c638ba16b4a7b7ab
Nicolas <nicolasoutog@gmail.com>
2022-12-26 13:32:47 -0300

    Refact into react components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import './App.css';
+import Header from './components/Header.js';
+import ToolBar from './components/TollBar.js';
+import MainContent from './components/MainContent.js';
 
 function App() {
   return (
     <main className='App'>
-      <header>
-        <div className='navBar'>
-          <h1 className='title'>asa</h1>
-        </div>
-      </header>
-
-      <section className='toolbar'>
-        toolbar
-      </section>
-
-      <section className='hot-posts'>
-        <div className='hot-post'>
-          hot post
-        </div>
-        <div className='hot-post'>
-          hot post
-        </div>
-        <div className='hot-post'>
-          hot post
-        </div>
-        <div className='hot-post'>
-          hot post
-        </div>
-      </section>
-
-      <section className='post'>
-        post
-      </section>
-      
-      <section className='bar'>
-        bar
-      </section>
+      <Header />
+      <ToolBar />
+      <MainContent />
     </main>
     
   );
 }
-
-/*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
 
 export default App;
