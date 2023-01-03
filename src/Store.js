794cb84fc3152e0ca93a8b00d0513442f1676c05
Nicolas <nicolasoutrog@gmail.com>
2023-01-02 21:45:30 -0300

    Fix typo

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./api/PostsSlice.js";
-import newPostsReducer from "./api/HotPostsSlice.js";
+import newPostsReducer from "./api/NewPostsSlice.js";
 
 export default configureStore({
     reducer: {
