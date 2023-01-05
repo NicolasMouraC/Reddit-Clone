5554b7fa3f06d1dcfd43c5b6df32a8f1d1e05a9b
Nicolas de Moura <97635109+NicolasMouraC@users.noreply.github.com>
2023-01-05 18:30:58 -0300

    Work on sidebar section - API integration (#3) (#4)
    * Working on toolbar
    
    * Working
    
    * Work
    
    * Finish sidebar section with API integration
    
    Co-authored-by: Nicolas <nicolasoutog@gmail.com>
    
    Co-authored-by: Nicolas <nicolasoutog@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { BsReddit } from 'react-icons/bs'
+import { useDispatch, useSelector } from "react-redux";
+import { selectTopic } from "../slices/TopicsSlice.js";
+import { toggleIsLoaded, getPost } from "../slices/PostsSlice.js";
+import { fetchData } from '../Utils.js'
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const inputRef = useRef(null);
+    const topic = useSelector(selectTopic);
+
+    const handleChange = async () => {
+        const searchTerm = inputRef.current.value;
+        dispatch(toggleIsLoaded);
+
+        if (searchTerm.length === 0) {
+            const posts = await fetchData(`https://www.reddit.com/r/${topic}.json`);
+            dispatch(getPost({ posts: posts }));
+            return;
+        }
+        
+        const posts = await fetchData(`https://www.reddit.com/search.json?q=${searchTerm}`);
+        dispatch(getPost({posts: posts}));
+    }
+
     return (
         <header>
             <Container fluid='true' />
@@ -14,7 +36,12 @@ const Header = () => {
                         <h1 className="title">Redd<span className="minimal">i</span>t <span className="minimal">Minimal</span></h1>
                     </Col>
                     <Col className="header-input" md={8} sm={12} >
-                        <input placeholder="Search Reddit Minimal" type='text' className="searchBar" />
+                        <input
+                            ref={inputRef}
+                            placeholder="Search Reddit Minimal"
+                            onChange={handleChange} 
+                            type='text' 
+                            className="searchBar" />
                     </Col>
                 </Row>
       </header>
